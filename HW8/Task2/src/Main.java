d131a8c4c0e56c4fbc43ee831058a4755a05795f
harderu <live.imho@gmail.com>
2025-02-16 18:35:43 +0300

    1

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        int [] people = new int[10];
+        Random random = new Random();
+
+
+        for (int i = 0 ; i < people.length; i++) {
+            people[i] = random.nextInt(100);
+        }
+
+            for (int i = 0 ; i < people.length; i++){
+                System.out.println(people[i]);
+            }
+        }
+
+
+
+
     }
-}
+
